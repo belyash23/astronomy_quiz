99faec5a5e73d51bafdc8a2c8b1457835e9d303a
Anton Belyaev <belyash23@inbox.ru>
2021-03-25 11:34:11 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <stage/>
+</template>
+
+<script>
+import Stage from './components/Stage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Stage
+  }
+}
+</script>
---
+++ src/components/Stage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="stage">
+      <div class="question">{{ question }}</div>
+      <answer></answer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stage',
+  props: {
+    question: String,
+    answers: Array
+  }
+}
+</script>
