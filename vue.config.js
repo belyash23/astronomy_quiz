f6a440198bbacee02ab1727b7660ccf45466434f
Anton Belyaev <belyash23@inbox.ru>
2021-03-26 13:09:30 +0400

    add deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,8 @@ module.exports = {
   },
   chainWebpack: config => {
     config.module.rules.delete('eslint');
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/astronomy_quiz/'
+    : '/'
 }
