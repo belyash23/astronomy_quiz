bfa13d045208c22f4605ba19b4de2e30ecb834c8
belyash23 <59472746+belyash23@users.noreply.github.com>
2021-04-07 21:18:52 +0400

    Update earth-ru.js

--- src/quizzes/earth/earth-ru.js
+++ src/quizzes/earth/earth-ru.js
@@ -23,94 +23,14 @@ export default {
       ]
     },
     {
-      question: '1+3=',
+      question: 'текст вопроса',
       difficult: 1,
       answers: [
         {
-          answer: '1',
+          answer: 'ответ 1',
           correct: false
-        },
-        {
-          answer: '2',
-          correct: false
-        },
-        {
-          answer: '3',
-          correct: true
         }
       ]
-    },
-    {
-      question: '1+4=',
-      difficult: 1,
-      answers: [
-        {
-          answer: '1',
-          correct: false
-        },
-        {
-          answer: '2',
-          correct: false
-        },
-        {
-          answer: '3',
-          correct: true
-        }
-      ]
-    },
-    {
-      question: '1+5=',
-      difficult: 2,
-      answers: [
-        {
-          answer: '1',
-          correct: false
-        },
-        {
-          answer: '2',
-          correct: false
-        },
-        {
-          answer: '3',
-          correct: true
-        }
-      ]
-    },
-    {
-      question: '1+6=',
-      difficult: 3,
-      answers: [
-        {
-          answer: '1',
-          correct: false
-        },
-        {
-          answer: '2',
-          correct: false
-        },
-        {
-          answer: '3',
-          correct: true
-        }
-      ]
-    },
-    {
-      question: '1+7=',
-      difficult: 2,
-      answers: [
-        {
-          answer: '1',
-          correct: false
-        },
-        {
-          answer: '2',
-          correct: false
-        },
-        {
-          answer: '3',
-          correct: true
-        }
-      ]
-    },
+    }
   ]
 }
