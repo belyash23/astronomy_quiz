63d393b723a6dd9fafe6cca2daa7148e3a4d940c
SalekDm <81669806+SalekDm@users.noreply.github.com>
2021-04-07 22:16:41 +0400

    Update earth-ru.js

--- src/quizzes/earth/earth-ru.js
+++ src/quizzes/earth/earth-ru.js
@@ -5,11 +5,11 @@ export default {
   miniature: require('./background.jpeg'),
   questions: [
     {
-      question: '1+2=',
+      question: 'Какая по счёту планета Земля от Солнца?',
       difficult: 1,
       answers: [
         {
-          answer: '1',
+          answer: '4',
           correct: false
         },
         {
@@ -23,14 +23,256 @@ export default {
       ]
     },
     {
-      question: 'текст вопроса',
+      question: 'Какую форму имеет планета Земля?',
       difficult: 1,
       answers: [
         {
-          answer: 'ответ 1',
+          answer: 'Форму шара',
+          correct: false
+        },
+        {
+          aswer: 'Форму эллипса',
+          correct: false
+        },
+        {
+          answer: 'Форму сфероида',
+          correct: true
+        }
+      ] 
+    }, 
+    {
+      question: 'Назовите жидкую водную оболочку Земли',
+      difficult: 1,
+      answers: [
+        {
+          answer: 'Атмосфера',
+          correct: false
+        },
+        {
+          aswer: 'Гидросфера',
+          correct: true
+        },
+        {
+          answer: 'Литосфера',
           correct: false
         }
-      ]
-    }
-  ]
-}
+      ] 
+    }, 
+    {
+      question: 'Назовите газовую оболочку земли',
+      difficult: 1,
+      answers: [
+        {
+          answer: 'Атмосфера',
+          correct: true
+        },
+        {
+          aswer: 'Гидросфера',
+          correct: false
+        },
+        {
+          answer: 'Литосфера',
+          correct: false
+        }
+      ] 
+    }, 
+    {
+      question: 'Из скольких процентов азота состоит атмосфера?',
+      difficult: 1,
+      answers: [
+        {
+          answer: '56%',
+          correct: false
+        },
+        {
+          aswer: '83%',
+          correct: false
+        },
+        {
+          answer: '78%',
+          correct: true
+        }
+      ] 
+    }, 
+    {
+      question: 'Назовите приблизительно массу Земли',
+      difficult: 2,
+      answers: [
+        {
+          answer: '7 * 1020 кг',
+          correct: false
+        },
+        {
+          aswer: '4 * 1030 кг',
+          correct: false
+        },
+        {
+          answer: '6 * 1024 кг',
+          correct: true
+        }
+      ] 
+    }, 
+    {
+      question: 'Какой процесс возникает из – за наличия газов в атмосфере?',
+      difficult: 2,
+      answers: [
+        {
+          answer: 'Парниковый эффект',
+          correct: true
+        },
+        {
+          aswer: 'Солнечный эффект',
+          correct: false
+        },
+        {
+          answer: 'Газовый эффект',
+          correct: false
+        }
+      ] 
+    }, 
+    {
+      question: 'Назовите среднюю годовую температуру Земли',
+      difficult: 2,
+      answers: [
+        {
+          answer: '15 °С',
+          correct: true
+        },
+        {
+          aswer: '20 °С',
+          correct: false
+        },
+        {
+          answer: '10 °С',
+          correct: false
+        }
+      ] 
+    }, 
+    {
+      question: 'Назовите средний радиус Земли',
+      difficult: 2,
+      answers: [
+        {
+          answer: '6058 км',
+          correct: false
+        },
+        {
+          aswer: '6378 км',
+          correct: true
+        },
+        {
+          answer: '6431 км',
+          correct: false
+        }
+      ] 
+    }, 
+    {      
+      question: 'Назовите среднюю плотность Земли',
+      difficult: 2,
+      answers: [
+        {
+          answer: '5200 кг/см3',
+          correct: false
+        },
+        {
+          aswer: '5520 кг/см3',
+          correct: true
+        },
+        {
+          answer: '6120 кг/см3',
+          correct: false
+        }
+      ] 
+    }, 
+    {      
+      question: 'На сколько градусов была бы ниже температура Земли, если бы в атмосфере не было углекислого газа и водяных паров?',
+      difficult: 3,
+      answers: [
+        {
+          answer: 'На 10 °С',
+          correct: false
+        },
+        {
+          aswer: 'На 100 °С',
+          correct: false
+        },
+        {
+          answer: 'На 40 °С',
+          correct: true
+        }
+      ] 
+    }, 
+    {      
+      question: 'Назовите толщину Земли под океаном',
+      difficult: 3,
+      answers: [
+        {
+          answer: '6 – 10 км',
+          correct: true
+        },
+        {
+          aswer: '8 -12 км',
+          correct: false
+        },
+        {
+          answer: '9 – 16 км',
+          correct: false
+        }
+      ] 
+    }, 
+    {      
+      question: 'В каком состоянии находится вещество во внутреннем ядре?',
+      difficult: 3,
+      answers: [
+        {
+          answer: 'В газообразном',
+          correct: false
+        },
+        {
+          aswer: 'В жидком',
+          correct: false
+        },
+        {
+          answer: 'В твердом',
+          correct: true
+        }
+      ] 
+    }, 
+    {
+      question: 'Назовите процент углекислого газа в атмосфере',
+      difficult: 3,
+      answers: [
+        {
+          answer: '0,095 %',
+          correct: false
+        },
+        {
+          aswer: '3,420 %',
+          correct: false
+        },
+        {
+          answer: '0,045 %',
+          correct: true
+        }
+      ] 
+    }, 
+    {
+      question: 'Где находится южный полюс Земли?',
+      difficult: 3,
+      answers: [
+        {
+          answer: 'На юге России',
+          correct: false
+        },
+        {
+          aswer: 'На севере Исландии',
+          correct: false
+        },
+        {
+          answer: 'На севере Канады',
+          correct: true
+        },
+      ] 
+    } 
+  ] 
+
